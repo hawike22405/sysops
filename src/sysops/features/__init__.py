bf8d2953f626314f7daeeb000b314a419de579cb
Sayan Pal <gamesrfun22405@gmail.com>
2026-08-21 15:27:01 +0530

    feat: export htop live view

--- src/sysops/features/__init__.py
+++ src/sysops/features/__init__.py
@@ -2,11 +2,13 @@
 
 from .achievements import check_achievements, list_all_badges
 from .benchmark import run_benchmark
+from .htop_view import run_htop_view
 from .interactive_menu import run_interactive_menu
 
 __all__ = [
     "check_achievements",
     "list_all_badges",
     "run_benchmark",
+    "run_htop_view",
     "run_interactive_menu",
 ]
