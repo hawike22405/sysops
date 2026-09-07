a85e047728e1fa51fb5d3a8bdc6bed371c558d1b
Sayan Pal <gamesrfun22405@gmail.com>
2026-09-07 16:59:53 +0530

    chore: expose OS logo animation API

--- src/sysops/features/__init__.py
+++ src/sysops/features/__init__.py
@@ -4,8 +4,13 @@ from .achievements import check_achievements, list_all_badges
 from .benchmark import run_benchmark
 from .htop_view import run_htop_view
 from .interactive_menu import run_interactive_menu
+from .os_logo import animate_os_logo, detect_os, logo_frames, render_os_frame
 
 __all__ = [
+    "animate_os_logo",
+    "detect_os",
+    "logo_frames",
+    "render_os_frame",
     "check_achievements",
     "list_all_badges",
     "run_benchmark",
